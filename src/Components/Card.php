2e7d32327b8ee6b48463b23ea73ad8823b46f2ea
Syed Irfaq R. ⚡️ <irazasyed@users.noreply.github.com>
2016-08-23 14:19:06 +0530

    Applied fixes from StyleCI (#15)

--- src/Components/Card.php
+++ src/Components/Card.php
@@ -110,7 +110,7 @@ class Card implements \JsonSerializable
      */
     public function toArray()
     {
-        if (!isset($this->payload['title'])) {
+        if (! isset($this->payload['title'])) {
             throw CouldNotCreateCard::titleNotProvided();
         }
 
