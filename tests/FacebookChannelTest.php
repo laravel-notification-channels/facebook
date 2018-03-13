8ec697304bec77fb10212e5c17cd2660051a7f61
Brandon Shar <6599653+BrandonShar@users.noreply.github.com>
2018-03-12 23:40:33 -0400

    update style

--- tests/FacebookChannelTest.php
+++ tests/FacebookChannelTest.php
@@ -2,10 +2,10 @@
 
 namespace NotificationChannels\Facebook\Test;
 
-use GuzzleHttp\Client as HttpClient;
-use Illuminate\Container\Container;
-use Illuminate\Notifications\Notification;
 use Mockery;
+use Illuminate\Container\Container;
+use GuzzleHttp\Client as HttpClient;
+use Illuminate\Notifications\Notification;
 use NotificationChannels\Facebook\FacebookChannel;
 use NotificationChannels\Facebook\FacebookMessage;
 
@@ -14,7 +14,6 @@ class FacebookChannelTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function you_can_swap_the_underlying_facebook_instance_with_a_from_parameter()
     {
-
         $message = FacebookMessage::create('my text')->to('12345')->from('abc123');
         $mock_notification = Mockery::mock(Notification::class, function ($m) use ($message) {
             $m->shouldReceive('toFacebook')->with('notifiable')->andReturn($message);
